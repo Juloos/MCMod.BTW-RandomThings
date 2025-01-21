63c72aa20f12040105b10d54f9060cce88d923b2
Juloos <juloos.sfr@gmail.com>
2025-01-21 14:57:42 +0100

    :bug: fix keybindings resetting to default

---
+++ src/main/java/net/random/things/mixin/GameSettingsMixin.java
@@ -0,0 +1,19 @@
+package net.random.things.mixin;
+
+import btw.random.things.RandomThingsAddon;
+import net.minecraft.src.GameSettings;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameSettings.class)
+public class GameSettingsMixin {
+    @Inject(
+            method = "<init>(Lnet/minecraft/src/Minecraft;Ljava/io/File;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/src/GameSettings;loadOptions()V")
+    )
+    private void initMixin(CallbackInfo ci) {
+        RandomThingsAddon.getInstance().initKeybind((GameSettings) (Object) this);
+    }
+}
