63d61b89c005d168e204f5ce8dd6ad9f3b47b6d0
Juloos <juloos.sfr@gmail.com>
2025-02-01 14:10:39 +0100

    :bug: incorrect slot keys when in GUIs

---
+++ src/main/java/net/random/things/mixin/GuiContainerMixin.java
@@ -0,0 +1,35 @@
+package net.random.things.mixin;
+
+import btw.random.things.RandomThingsAddon;
+import net.minecraft.src.GuiContainer;
+import net.minecraft.src.GuiScreen;
+import net.minecraft.src.Slot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(GuiContainer.class)
+public abstract class GuiContainerMixin extends GuiScreen {
+    @Shadow protected abstract void handleMouseClick(Slot par1Slot, int par2, int par3, int par4);
+
+    @Shadow private Slot theSlot;
+
+    @Inject(
+            method = "checkHotbarKeys",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void checkHotbarKeys(int key, CallbackInfoReturnable<Boolean> cir) {
+        if (this.mc.thePlayer.inventory.getItemStack() == null && this.theSlot != null) {
+            for (int i = 0; i < 9; i++) {
+                if (key == RandomThingsAddon.slot_keys[i].keyCode) {
+                    this.handleMouseClick(this.theSlot, this.theSlot.slotNumber, i, 2);
+                    cir.setReturnValue(true);
+                }
+            }
+        }
+        cir.setReturnValue(false);
+    }
+}
