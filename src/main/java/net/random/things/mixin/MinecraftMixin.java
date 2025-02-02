72973156efdf74b5c167dd810bdbcea01f791987
Juloos <juloos.sfr@gmail.com>
2025-02-02 14:01:16 +0100

    :bug: incorrect mouse events logic

--- src/main/java/net/random/things/mixin/MinecraftMixin.java
+++ src/main/java/net/random/things/mixin/MinecraftMixin.java
@@ -1,7 +1,6 @@
 package net.random.things.mixin;
 
 import net.minecraft.src.EntityClientPlayerMP;
-import org.lwjgl.input.Keyboard;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -26,34 +25,8 @@ public abstract class MinecraftMixin {
     @Unique private boolean wasZooming = false;
 
     @Redirect(method = "runTick", at = @At(value = "INVOKE", target = "Lorg/lwjgl/input/Keyboard;getEventKey()I", ordinal = 13))
-    private int redirectF5Call() {
-        if (RandomThingsAddon.first_person_key.isPressed())
-            gameSettings.thirdPersonView = 0;
-        else if (RandomThingsAddon.third_person_key.isPressed())
-            gameSettings.thirdPersonView = 1;
-        else if (RandomThingsAddon.backwards_facing_key.isPressed())
-            gameSettings.thirdPersonView = 2;
-        else if (RandomThingsAddon.f5_key.isPressed())
-            return Keyboard.KEY_F5;
+    private int removeVanillaF5Call() {
         return -1;
-    }
-
-    @Inject(
-            method = "runTick",
-            at = @At("HEAD")
-    )
-    private void handleZooming(CallbackInfo ci) {
-        if (Keyboard.isKeyDown(RandomThingsAddon.zoom_key.keyCode) && !wasZooming) {
-            oldFovSetting = this.gameSettings.fovSetting;
-            this.gameSettings.fovSetting = RandomThingsAddon.zoomFov;
-            this.gameSettings.smoothCamera = true;
-            wasZooming = true;
-        }
-        if (!Keyboard.isKeyDown(RandomThingsAddon.zoom_key.keyCode) && wasZooming) {
-            this.gameSettings.fovSetting = oldFovSetting;
-            this.gameSettings.smoothCamera = false;
-            wasZooming = false;
-        }
     }
 
     @Redirect(method = "runTick", at = @At(value = "INVOKE", target = "Lorg/lwjgl/input/Keyboard;getEventKey()I", ordinal = 15))
@@ -61,10 +34,34 @@ public abstract class MinecraftMixin {
         return -1;
     }
 
-    @Inject(method = "runTick", at = @At(value = "INVOKE", target = "Lorg/lwjgl/input/Keyboard;getEventKey()I", ordinal = 15, shift = At.Shift.BY, by = -9))
+    @Inject(method = "runTick", at = @At(value = "FIELD", target = "net/minecraft/src/GameSettings.chatVisibility : I"))
     private void handleSlotKeys(CallbackInfo ci) {
+        // Perspective keybindings
+        if (RandomThingsAddon.first_person_key.isPressed())
+            gameSettings.thirdPersonView = 0;
+        else if (RandomThingsAddon.third_person_key.isPressed())
+            gameSettings.thirdPersonView = 1;
+        else if (RandomThingsAddon.backwards_facing_key.isPressed())
+            gameSettings.thirdPersonView = 2;
+        else if (RandomThingsAddon.f5_key.isPressed())
+            gameSettings.thirdPersonView = (gameSettings.thirdPersonView + 1) % 3;
+
+        // Slots keybindings
         for (int i = 0; i < RandomThingsAddon.slot_keys.length; i++)
             if (RandomThingsAddon.slot_keys[i].isPressed())
                 this.thePlayer.inventory.currentItem = i;
+
+        // Zoom keybinding
+        if (RandomThingsAddon.zoom_key.pressed && !wasZooming) {
+            oldFovSetting = this.gameSettings.fovSetting;
+            this.gameSettings.fovSetting = RandomThingsAddon.zoomFov;
+            this.gameSettings.smoothCamera = true;
+            wasZooming = true;
+        }
+        if (!RandomThingsAddon.zoom_key.pressed && wasZooming) {
+            this.gameSettings.fovSetting = oldFovSetting;
+            this.gameSettings.smoothCamera = false;
+            wasZooming = false;
+        }
     }
 }
